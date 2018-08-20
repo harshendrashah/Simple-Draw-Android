898fc514beec981d1b139f096418cc9dfad7b083
harshendrashah <harshendrashah812@gmail.com>
2018-08-20 11:49:16 +0530

    Added Custom View

---
+++ app/src/main/java/com/example/harshendra/simpledraw/SimpleDrawingView.java
@@ -0,0 +1,13 @@
+package com.example.harshendra.simpledraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SimpleDrawingView extends View {
+
+    public SimpleDrawingView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+}
