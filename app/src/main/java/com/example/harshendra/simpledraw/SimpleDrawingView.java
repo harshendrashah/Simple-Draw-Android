aec6c4e4ce899dfa7030e2b13f2eb90e36c06ecd
harshendrashah <harshendrashah812@gmail.com>
2018-08-20 12:10:50 +0530

    Handled Touch Interactions

--- app/src/main/java/com/example/harshendra/simpledraw/SimpleDrawingView.java
+++ app/src/main/java/com/example/harshendra/simpledraw/SimpleDrawingView.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleDrawingView extends View {
 
@@ -13,12 +18,34 @@ public class SimpleDrawingView extends View {
     private final int paintColor = Color.BLACK;
     // Define Paint and Canvas
     private Paint drawPaint;
+    // Stores Points to draw circles each time user touches
+    private List<Point> circlePoints;
 
     public SimpleDrawingView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         setFocusable(true);
         setFocusableInTouchMode(true);
         setupPaint();
+        circlePoints = new ArrayList<>();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (Point p : circlePoints) {
+            canvas.drawCircle(p.x, p.y, 5, drawPaint);
+        }
+    }
+
+    // Append new circle each time user presses on screen
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        circlePoints.add(new Point(Math.round(touchX), Math.round(touchY)));
+        // indicate view should be redrawn
+        postInvalidate();
+        return true;
     }
 
     // Setup Paint with color and stroke style
@@ -32,12 +59,4 @@ public class SimpleDrawingView extends View {
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(50, 50, 20, drawPaint);
-        drawPaint.setColor(Color.GREEN);
-        canvas.drawCircle(50, 150, 20, drawPaint);
-        drawPaint.setColor(Color.BLUE);
-        canvas.drawCircle(50, 250, 20, drawPaint);
-    }
 }
