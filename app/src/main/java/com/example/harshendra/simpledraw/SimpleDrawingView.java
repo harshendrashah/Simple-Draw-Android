4847303a523047640e7790c231b7ec27565307c4
harshendrashah <harshendrashah812@gmail.com>
2018-08-20 11:58:18 +0530

    Setup Paint with canvas and stroke styles

--- app/src/main/java/com/example/harshendra/simpledraw/SimpleDrawingView.java
+++ app/src/main/java/com/example/harshendra/simpledraw/SimpleDrawingView.java
@@ -1,13 +1,43 @@
 package com.example.harshendra.simpledraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class SimpleDrawingView extends View {
 
+    // Initial Color
+    private final int paintColor = Color.BLACK;
+    // Define Paint and Canvas
+    private Paint drawPaint;
+
     public SimpleDrawingView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
     }
 
+    // Setup Paint with color and stroke style
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawCircle(50, 50, 20, drawPaint);
+        drawPaint.setColor(Color.GREEN);
+        canvas.drawCircle(50, 150, 20, drawPaint);
+        drawPaint.setColor(Color.BLUE);
+        canvas.drawCircle(50, 250, 20, drawPaint);
+    }
 }
