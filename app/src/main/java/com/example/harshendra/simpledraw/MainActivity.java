e0a2bd171bc47d75d7055cda8c4b2e2278f80014
harshendrashah <harshendrashah812@gmail.com>
2018-08-21 18:31:36 +0530

    Added Bottom Appbar

--- app/src/main/java/com/example/harshendra/simpledraw/MainActivity.java
+++ app/src/main/java/com/example/harshendra/simpledraw/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.harshendra.simpledraw;
 
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomAppBar bottomAppBar;
+    ActionBar toolbar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomAppBar = findViewById(R.id.bottomAppbar);
+
+        bottomAppBar.replaceMenu(R.menu.drawing_menu);
+
+        //toolbar = getSupportActionBar();
+
+        //bottomNavigationView = findViewById(R.id.navigationView);
+
     }
 }
