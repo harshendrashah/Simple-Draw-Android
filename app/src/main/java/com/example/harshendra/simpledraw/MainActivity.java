db7bc5fef11c3a35cedc9e39649d4030a496e281
harshendrashah <harshendrashah812@gmail.com>
2018-08-22 10:40:36 +0530

    Updates

--- app/src/main/java/com/example/harshendra/simpledraw/MainActivity.java
+++ app/src/main/java/com/example/harshendra/simpledraw/MainActivity.java
@@ -6,13 +6,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomAppBar bottomAppBar;
-    ActionBar toolbar;
-    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomAppBar = findViewById(R.id.bottomAppbar);
+        setSupportActionBar(bottomAppBar);
 
-        bottomAppBar.replaceMenu(R.menu.drawing_menu);
+    }
 
-        //toolbar = getSupportActionBar();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bottom_appbar_menu, menu);
+        return true;
+    }
 
-        //bottomNavigationView = findViewById(R.id.navigationView);
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.appBarSave:
+                Toast.makeText(this, "Save clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.appBarColor:
+                Toast.makeText(this, "Color clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.appBarClear:
+                Toast.makeText(this, "Clear clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.appBarShapes:
+                Toast.makeText(this, "Shapes clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.appBarUndo:
+                Toast.makeText(this, "Undo clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return true;
+        }
+        return true;
     }
 }
